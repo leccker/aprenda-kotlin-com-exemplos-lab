ca0c8ab52b8d3ac764c2413a65142ca8e09a64ee
leccker <144380558+leccker@users.noreply.github.com>
2023-10-04 19:10:17 -0300

    ajustes

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,42 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+class Usuario(val nome: String)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+class Formacao(val nome: String, val nivel: Nivel, val conteudos: List<ConteudoEducacional>) {
 
-    val inscritos = mutableListOf<Usuario>()
+    val alunosMatriculados = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricularAluno(usuario: Usuario) {
+        alunosMatriculados.add(usuario)
+        println("${usuario.nome} foi matriculado na formação de $nome.")
+    }
+
+    fun listarConteudos() {
+        println("Conteúdos da formação de $nome (Nível: $nivel):")
+        for (conteudo in conteudos) {
+            println("- ${conteudo.nome} (Duração: ${conteudo.duracao} minutos)")
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    // Criação de objetos para teste
+    val usuario1 = Usuario("João")
+    val usuario2 = Usuario("Maria")
+
+    val conteudo1 = ConteudoEducacional("Introdução à Programação", 90)
+    val conteudo2 = ConteudoEducacional("Banco de Dados", 120)
+    val conteudo3 = ConteudoEducacional("Segurança da Informação", 150)
+
+    val formacao1 = Formacao("Desenvolvedor Full Stack", Nivel.INTERMEDIARIO, listOf(conteudo1, conteudo2))
+    val formacao2 = Formacao("Engenheiro de Segurança", Nivel.DIFICIL, listOf(conteudo1, conteudo3))
+
+    formacao1.matricularAluno(usuario1)
+    formacao2.matricularAluno(usuario2)
+
+    formacao1.listarConteudos()
+    formacao2.listarConteudos()
 }
